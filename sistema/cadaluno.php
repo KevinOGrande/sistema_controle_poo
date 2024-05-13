b5826f8343363e688f5203ba8724d402bc58f5d4
Kevin Pancini <kevindornas44@gmail.com>
2024-05-13 11:45:18 -0300

    front

--- sistema/cadaluno.php
+++ sistema/cadaluno.php
@@ -3,20 +3,75 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:wght@100&display=swap" rel="stylesheet">
     <title>Document</title>
+    <style>
+        header{
+            background-color: #1B62B7;
+        }
+        .mb-3{
+            width:20%;
+        }
+        .formulario{
+            display:flex;
+            flex-direction:column;
+            align-items: center;
+            justify-content: center;
+        }
+        .form-select{
+            width:100%;
+        }
+        .botao{
+            margin-top: 1%;
+            align-items: center;
+        }
+        .fs-1{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <form action="cadaluno.php" method="get">
-        <input type="text" name="matricula" id="matricula">
-        <input type="text" name="nome" id="nome">
-        <input type="text" name="telefone" id="telefone">
-        <input type="text" name="usuario" id="usuario">
-        <input type="password" name="senha" id="senha">
-        <select name="status" id="status">
-            <option value="aluno">Aluno</option>
-            <option value="ex_aluno">Ex Aluno</option>
-        </select>
-        <input type="submit" value="cadastrar">
+    <header>
+        <nav class="navbar body-tertiary">
+            <img src="image/senai_logo1.png" alt="">
+        </nav>
+    </header>
+    <p class="fs-1">Cadastro de Aluno</p>
+    <form action="cadaluno.php" method="get" class="formulario">
+        <div class="mb-3">
+            <label class="form-label">Matricula:</label>
+            <input type="text" name="matricula" id="matricula" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Nome:</label>
+            <input type="text" name="nome" id="nome" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Telefone:</label>
+            <input type="text" name="telefone" id="telefone" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Usuario:</label>
+            <input type="text" name="usuario" id="usuario" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Senha:</label>
+            <input type="text" name="senha" id="senha" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div>
+            <select name="status" id="status" class="form-select">
+                <option>Aluno ou Ex-Aluno?</option>
+                <option value="aluno">Aluno</option>
+                <option value="ex_aluno">Ex Aluno</option>
+            </select>
+        </div>
+        <div class="botao">
+            <input type="submit" value="cadastrar" class="btn btn-success">
+        </div>
+        
     </form>
 </body>
 </html>
--- sistema/login.php
+++ sistema/login.php
@@ -7,15 +7,60 @@ session_destroy();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <style>
+            header{
+                background-color: #1B62B7;
+            }
+            .mb-3{
+                width:20%;
+            }
+            .formulario{
+                display:flex;
+                flex-direction:column;
+                align-items: center;
+                justify-content: center;
+            }
+            .botao{
+                margin-top: 1%;
+                align-items: center;
+            }
+            .fs-1{
+                text-align: center;
+            }
+            #cad{
+                align-items: center;
+            }
+        </style>
         <title>Document</title>
     </head>
     <body>
-        <form action="login.php" method="post">
-            <input type="text" name="user">
-            <input type="password" name="senha">
-            <input type="submit" value="enviar">
+        <header>
+            <nav class="navbar body-tertiary">
+                <img src="image/senai_logo1.png" alt="">
+            </nav>
+        </header>
+        <p class="fs-1">Login</p>
+        <form action="login.php" method="post" class="formulario">
+            <div class="mb-3">
+                <label class="form-label">Usuario:</label>
+                <input type="text" name="user" id="usuario" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Senha:</label>
+                <input type="password" name="senha" id="senha" required class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="botao">
+                <input type="submit" value="Entrar" class="btn btn-primary">
+            </div>
         </form>
-        <a href="cadaluno.php">Cadastro de aluno</a>
+        <div>
+            <a href="cadaluno.php" id="cad">Cadastro de aluno</a>
+        </div>
+        
     </body>
     </html>
 <?php
